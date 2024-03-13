c1f6b1aa0d5a760a64a490ada55411c287d77a77
pau12llm <pau.llort@students.salle.url.edu>
2024-03-13 19:22:27 +0100

    homeview canviat

--- Battle_ArenaJP/src/views/HomeView.vue
+++ Battle_ArenaJP/src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home-page">
+    <div class="banner">
+      <h1>BATTLE ARENA</h1>
+      <button @click="goToLogin">LOG IN</button>
+      <button @click="goToRegister">Register NOW</button>
+    </div>
+    <!-- Coloca aquí más contenido de la página de inicio si es necesario -->
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      this.$router.push('/login');
+    },
+    goToRegister() {
+      this.$router.push('/register');
+    }
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.home-page .banner {
+  /* Agrega estilos para el banner aquí */
+}
+</style>
