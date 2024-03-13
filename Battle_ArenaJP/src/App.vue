7ee2e1f9ca1c7a3beeca7a011cd76877dc28e3f9
pau12llm <pau.llort@students.salle.url.edu>
2024-03-13 20:11:57 +0100

    appVuefet

--- Battle_ArenaJP/src/App.vue
+++ Battle_ArenaJP/src/App.vue
@@ -1,29 +1,3 @@
 <template>
-  <div class="home-page">
-    <div class="banner">
-      <h1>BATTLE ARENA</h1>
-      <button @click="goToLogin">LOG IN</button>
-      <button @click="goToRegister">Register NOW</button>
-    </div>
-    <!-- Coloca aquí más contenido de la página de inicio si es necesario -->
-  </div>
+    <router-view/>
 </template>
-
-<script>
-export default {
-  methods: {
-    goToLogin() {
-      this.$router.push('/login');
-    },
-    goToRegister() {
-      this.$router.push('/register');
-    }
-  }
-}
-</script>
-
-<style>
-.home-page .banner {
-  /* Agrega estilos para el banner aquí    edffdgf */
-}
-</style>
